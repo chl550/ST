0a0d5ad84b06ac5e51fc0c1221d7a9921af381aa
chl550 <chl550@ucsd.edu>
2018-06-13 15:48:16 -0700

    final

--- GPS.ino
+++ GPS.ino
@@ -19,9 +19,9 @@ char dest[64] = {0};
 int sensorValue = 0;
 float c = millis();
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   EEBlue.begin(9600);
-  //Serial.println("Bluetooth module open, use pasword 1234 to connect");
+  Serial.println("Bluetooth module open, use pasword 1234 to connect");
   ss.begin(GPSBaud);
   counter = 0;
   pinMode(2, INPUT);
@@ -50,46 +50,34 @@ void loop() {
   }*/
   //check if we receive a dollar sign signal
   
-  if (ss.available() > 0) {
+  while (ss.available() > 0) {
     gps.encode(ss.read());
     if (gps.location.isUpdated()) {
       
       float latitude = gps.location.lat();
       float longitude = gps.location.lng();
+      //for debug
       //float latitude = 32.17898;
       //float longitude = -117.28138;
       dtostrf(latitude, 6, 6, buf);
-      ss.end();
+
       strcat(dest, buf);
       strcat(dest, " ");
       dtostrf(longitude, 6, 5, buf2);
       strcat(dest, buf2);
       strcat(dest, "#");
       strcat(dest, "\n");
-
-      EEBlue.begin(9600);
+      Serial.println(dest);
       EEBlue.write(dest);
       for ( int i = 0; i < sizeof(dest);  ++i ) {
         dest[i] = (char)0;
       }
+      delay(10000);
       
     }
     
   }
-  EEBlue.write(EEBlue.available());
 
-  while (EEBlue.available() > 0) {
-    if (EEBlue.read() == "%") {
-      EEBlue.write("alert");
-    }
-    else if (EEBlue.read() == "v") {
-      ss.begin(9600);
-      break;
-    }
-      
-  }
- 
-    c = millis();
     
     
     
